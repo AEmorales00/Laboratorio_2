46f9bba7b7ba05fef166f97f7638bce873127e63
AEmorales00 <albertoenriquem98@gmail.com>
2022-08-22 22:45:08 -0600

    Login
    Se realizo la creación de la pantalla de usuario y contraseña y se agrega metodo para comparar la contraseña

---
+++ src/Clase_Negocio/Negocio.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clase_Negocio;
+
+/**
+ *
+ * @author elbet
+ */
+public class Negocio {
+    private int contraseña;
+    private String usuario;
+
+    public int getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(int contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    
+}
---
+++ src/laboratorio_2/Laboratorio_2.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package laboratorio_2;
+
+import Pantalla.JDlogin;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author elbet
+ */
+public class Laboratorio_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JDlogin a = new JDlogin(null, true);
+        a.setVisible(true);
+
+    }
+        
+    }
+    
+
